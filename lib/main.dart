66ebd729abe86e19b26715898d10398ca02dc3c2
Alvin Ramadhani <alvin080297@gmail.com>
2023-07-06 03:46:06 +0700

    Update : update main.dart
    - menggunakan MultiProvider
    - menambahkan 2 provider yang digunakan
    - mengimplementasikan initialRoute dan onGenerateRoute.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:daya_lima_test/providers/add_film_model.dart';
+import 'package:daya_lima_test/providers/film_model.dart';
+import 'package:daya_lima_test/screens/add_new_movie_screen.dart';
+import 'package:daya_lima_test/screens/home_screen.dart';
+import 'package:daya_lima_test/screens/movie_detail_screen.dart';
+import 'package:daya_lima_test/utils/color_palette.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +17,50 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => FilmProvider(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        ChangeNotifierProvider(
+          create: (context) => AddFilmProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Daya Lima',
+        theme: ThemeData(
+          primarySwatch: myPrimaryColor,
+          accentColor: Colors.blueGrey,
+        ),
+        initialRoute: '/',
+        onGenerateRoute: (RouteSettings settings) {
+          switch (settings.name) {
+            case '/':
+              return MaterialPageRoute(
+                builder: (BuildContext context) => const HomeScreen(),
+                settings: settings,
+              );
+            case MovieDetailScreen.routeName:
+              final Map<String, dynamic> args =
+                  settings.arguments as Map<String, dynamic>;
+              return MaterialPageRoute(
+                builder: (BuildContext context) => MovieDetailScreen(
+                  title: args['title'],
+                  description: args['description'],
+                  poster: args['poster'],
+                  createdBy: args['created_by'],
+                  createdDate: args['created_date'],
+                ),
+                settings: settings,
+              );
+            case AddMovieScreen.routeName:
+              return MaterialPageRoute(
+                builder: (BuildContext context) => AddMovieScreen(),
+                settings: settings,
+              );
+          }
+        },
       ),
     );
   }
